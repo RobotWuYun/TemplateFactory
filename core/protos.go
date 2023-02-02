345dbe1a32bd46001cb852681cfeaeaf93df0ceb
RobotWuYun <ai_robot@outlook.com>
2023-02-02 21:19:06 +0800

    add makestruct

--- core/protos.go
+++ core/protos.go
@@ -1,33 +1,96 @@
 package core
 
 import (
-	"github.com/golang/protobuf/protoc-gen-go/generator"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
-type netrpcPlugin struct{ *generator.Generator }
+const (
+	MessageFilePre = "message_"
+	ServiceFilePre = "service_"
+)
 
-func init() {
-    generator.RegisterPlugin(new(netrpcPlugin))
-}
+func GetMessage() {
+	plugin := getPlugin()
 
-func (p *netrpcPlugin) Name() string                { return "netrpc" }
-func (p *netrpcPlugin) Init(g *generator.Generator) { p.Generator = g }
+	for _, file := range plugin.Files {
+		// 通过文件名前缀区分服务和结构
+		if strings.HasPrefix(file.GeneratedFilenamePrefix, MessageFilePre) {
+			MakeStructsFromFile(file)
+		} else if strings.HasPrefix(file.GeneratedFilenamePrefix, ServiceFilePre) {
 
-func (p *netrpcPlugin) GenerateImports(file *generator.FileDescriptor) {
-	if len(file.Service) > 0 {
-		p.genImportCode(file)
+		}
+
+		//创建一个buf 写入生成的文件内容
+		var buf bytes.Buffer
+
+		// 写入go 文件的package名
+		pkg := fmt.Sprintf("package %s", file.GoPackageName)
+		buf.Write([]byte(pkg))
+		content := ""
+		//遍历消息,这个内容就是protobuf的每个消息
+		for _, msg := range file.Messages {
+			mapSrc := `
+                         newMap:=make(map[%v]%v)
+                     for k,v:=range x.%v {
+                        newMap[k]=v
+                     }
+                            x.%v=newMap
+                  `
+			//遍历消息的每个字段
+			for _, field := range msg.Fields {
+				//只有map 才这样做
+				if field.Desc.IsMap() {
+					content += fmt.Sprintf(mapSrc, field.Desc.MapKey().Kind().String(), field.Desc.MapValue().Kind().String(), field.GoName, field.GoName)
+				}
+			}
+			buf.Write([]byte(fmt.Sprintf(`
+           func (x *%s) CheckMap() {
+          %s
+           }`, msg.GoIdent.GoName, content)))
+		}
+		//指定输入文件名,输出文件名为demo.foo.go
+		filename := file.GeneratedFilenamePrefix + ".foo.go"
+		file := plugin.NewGeneratedFile(filename, ".")
+
+		// 将内容写入插件文件内容
+		file.Write(buf.Bytes())
 	}
-}
 
-func (p *netrpcPlugin) Generate(file *generator.FileDescriptor) {
-	for _, svc := range file.Service {
-		p.genServiceCode(svc)
+	// 生成响应
+	stdout := plugin.Response()
+	out, err := proto.Marshal(stdout)
+	if err != nil {
+		panic(err)
 	}
+
+	// 将响应写回标准输入, protoc会读取这个内容
+	fmt.Fprintf(os.Stdout, string(out))
 }
 
-func (p *netrpcPlugin) genImportCode(file *generator.FileDescriptor) {
-    p.P("// TODO: import code")
-}
+func getPlugin() (p *protogen.Plugin) {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
 
-func (p *netrpcPlugin) genServiceCode(svc *descriptor.ServiceDescriptorProto) {
-    p.P("// TODO: service code, Name = " + svc.GetName())
+	var req pluginpb.CodeGeneratorRequest
+	err = proto.Unmarshal(input, &req)
+	if err != nil {
+		panic(err)
+	}
+
+	opts := protogen.Options{}
+	p, err = opts.New(&req)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
--- core/struct.go
+++ core/struct.go
@@ -1,106 +1,13 @@
 package core
 
-import (
-	errs "TemplateFactory/error"
-	"TemplateFactory/utils"
-	"regexp"
-)
+import "google.golang.org/protobuf/compiler/protogen"
 
-type StrcutModel struct {
-	FilePath string
-	Data     string
-	Exist    bool
+func MakeStructsFromFile(file *protogen.File) {
+	for _, v := range file.Messages {
+		MakeStructsFromMessage(v)
+	}
 }
 
-func GetStructs(data string) (structs map[string]string, err error) {
-	structs = make(map[string]string)
-
-	re := regexp.MustCompile("type[\\s]+[\\w]+[\\s]+struct[\\s]*{")
-	found := re.FindAllString(data, -1)
-	if found == nil {
-		return
-	}
-
-	for _, key := range found {
-		if _, ok := structs[key]; ok {
-			err = errs.ErrStructNameExist
-			return
-		}
-		structs[key] = ""
-	}
-
-	for key := range structs {
-		var structData []rune
-		var index = utils.Utf8Index(data, key) + len(key)
-
-		var single = 1
-		for {
-			if single == 0 || index == (len(data)-1) {
-				break
-			}
-
-			charData := rune(data[index])
-			if charData == '{' {
-				single++
-			} else if charData == '}' {
-				single--
-			}
-			structData = append(structData, rune(data[index]))
-			index++
-		}
-		structs[key] = key + string(structData)
-	}
-	return
+func MakeStructsFromMessage(message *protogen.Message) string {
+	return ""
 }
-
-// func doStruct2pb(t reflect.Type) (res string) {
-// 	newStructs := make([]string, 0)
-// 	newFields := make([]string, 0)
-// 	for i := 0; i < t.NumField(); i++ {
-// 		f := t.Field(i)
-// 		name := f.Tag.Get("json")
-// 		var newType string
-// 		switch tp := f.Type.String(); tp {
-// 		case "string", "interface {}":
-// 			newType = "string"
-// 		case "[]string":
-// 			newType = "repeated string"
-// 		case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64":
-// 			newType = "int64"
-// 		case "time.Time":
-// 			newType = "google.protobuf.Timestamp"
-// 		case "float32", "float64":
-// 			newType = "float64"
-// 		case "bool":
-// 			newType = "bool"
-// 		default:
-// 			switch k := f.Type.Kind(); k {
-// 			case reflect.Struct:
-// 				if strings.Contains(strings.ToLower(f.Name), "image") {
-// 					newType = "Image"
-// 				} else {
-// 					sub := s.doStruct2pb(f.Type)
-// 					title := strings.Title(name)
-// 					newStructs = append(newStructs, fmt.Sprintf(`message %s {%s}`, title, sub))
-// 					newType = title
-// 				}
-// 			case reflect.Slice:
-// 				sub := s.doStruct2pb(f.Type.Elem())
-// 				title := strings.Title(strings.TrimRight(name, "s"))
-// 				newStructs = append(newStructs, fmt.Sprintf(`message %s {%s}`, title, sub))
-// 				newType = "repeated " + title
-// 			default:
-// 				fmt.Println("Unrecognized kind", k, f.Type.Kind())
-// 				continue
-// 			}
-// 		}
-// 		newFields = append(newFields, newType+" "+name)
-// 	}
-// 	for _, v := range newStructs {
-// 		res += v
-// 	}
-// 	for i, v := range newFields {
-// 		res += fmt.Sprintf(`%s = %d;`, v, (i + 1))
-// 	}
-// 	return
-// }
