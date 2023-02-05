98ff505cfc7c25d2af6072f739281f17ba75d09e
RobotWuYun <ai_robot@outlook.com>
2023-02-05 20:55:49 +0800

    2023.2.5

--- core/protos.go
+++ core/protos.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -23,45 +22,10 @@ func GetMessage() {
 	for _, file := range plugin.Files {
 		// 通过文件名前缀区分服务和结构
 		if strings.HasPrefix(file.GeneratedFilenamePrefix, MessageFilePre) {
-			MakeStructsFromFile(file)
+			MakeStructsFromFile(plugin, file)
 		} else if strings.HasPrefix(file.GeneratedFilenamePrefix, ServiceFilePre) {
-
+			continue
 		}
-
-		//创建一个buf 写入生成的文件内容
-		var buf bytes.Buffer
-
-		// 写入go 文件的package名
-		pkg := fmt.Sprintf("package %s", file.GoPackageName)
-		buf.Write([]byte(pkg))
-		content := ""
-		//遍历消息,这个内容就是protobuf的每个消息
-		for _, msg := range file.Messages {
-			mapSrc := `
-                         newMap:=make(map[%v]%v)
-                     for k,v:=range x.%v {
-                        newMap[k]=v
-                     }
-                            x.%v=newMap
-                  `
-			//遍历消息的每个字段
-			for _, field := range msg.Fields {
-				//只有map 才这样做
-				if field.Desc.IsMap() {
-					content += fmt.Sprintf(mapSrc, field.Desc.MapKey().Kind().String(), field.Desc.MapValue().Kind().String(), field.GoName, field.GoName)
-				}
-			}
-			buf.Write([]byte(fmt.Sprintf(`
-           func (x *%s) CheckMap() {
-          %s
-           }`, msg.GoIdent.GoName, content)))
-		}
-		//指定输入文件名,输出文件名为demo.foo.go
-		filename := file.GeneratedFilenamePrefix + ".foo.go"
-		file := plugin.NewGeneratedFile(filename, ".")
-
-		// 将内容写入插件文件内容
-		file.Write(buf.Bytes())
 	}
 
 	// 生成响应
--- core/struct.go
+++ core/struct.go
@@ -1,13 +1,37 @@
 package core
 
-import "google.golang.org/protobuf/compiler/protogen"
+import (
+	"bytes"
+	"fmt"
+	"strings"
 
-func MakeStructsFromFile(file *protogen.File) {
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func MakeStructsFromFile(plugin *protogen.Plugin, file *protogen.File) {
+	var structStrs []string
+
 	for _, v := range file.Messages {
-		MakeStructsFromMessage(v)
+		structStrs = append(structStrs, MakeStructsFromMessage(v))
 	}
+
+	var buf bytes.Buffer
+	pkg := fmt.Sprintf("package %s", file.GoPackageName)
+	buf.Write([]byte(pkg))
+	buf.Write([]byte(strings.Join(structStrs, "\n")))
+	filename := file.GeneratedFilenamePrefix + ".foo.go"
+	newfile := plugin.NewGeneratedFile(filename, ".")
+
+	// 将内容写入插件文件内容
+	newfile.Write(buf.Bytes())
 }
 
 func MakeStructsFromMessage(message *protogen.Message) string {
-	return ""
+	var fields []string
+	for _, field := range message.Fields {
+		fields = append(fields, fmt.Sprintf("%s\t%s", field.GoName, field.Desc.Kind().GoString()))
+		fmt.Println("TEST")
+	}
+	fieldStr := strings.Join(fields, "\n")
+	return fmt.Sprintf("type %s struct {/n%s}", message.GoIdent.GoName, fieldStr)
 }
